dbf2c510d5996ed7bba0b5c081e2aef641191adf
Vika <viktoriadruzenok@gmail.com>
2025-07-03 13:16:44 +0300

    Add: min

--- src/soup/main.py
+++ src/soup/main.py
@@ -1,5 +1,37 @@
+from pion import Pion,Spion
+import time
+
+
 def main():
-    print("Hello")
+    dron = Pion(
+        ip='10.1.100.134',
+        mavlink_port=5656,
+        logger=True
+    )
+
+    # dron.arm()
+    # dron.takeoff()
+    # time.sleep(4)
+    # start = 0
+    # n = 2 
+    # m =2
+    # for y in range(start,m):
+    #     dron.goto(start, y, 1, 0.0, wait=True)
+    #     time.sleep(4)
+    # for x in range(start-1,-n,-1):
+    #     dron.goto(x, m-1, 1, 0.0, wait=True)
+    #     time.sleep(4)
+    # for y in range(m-2,start,-1):
+    #     dron.goto(-n+1, y, 1, 0.0, wait=True)
+    #     time.sleep(4)
+    # for x in range (-n+1,start+1):
+    #     dron.goto(-n+1, start+1, 1, 0.0, wait=True)
+    #     time.sleep(4)
+
+    dron.land()
+    dron.stop()
+
+
 
 
 if __name__ == "__main__":
