357246af6f16c4b0cbc20fc3cc577bcbb0699661
Viktoria <Viktoriavo15@gmail.com>
2025-07-03 12:51:27 +0300

    skript dly Marka

--- src/soup/main.py
+++ src/soup/main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello")
+from pion import Pion
+import time
 
+def main():
+    # print("Hello")
+    drone = Pion(
+        ip = '10.1.100.134',
+        mavlink_port = 5656,
+        logger = True
+        )
+    drone.arm()
+    drone.takeoff()
+    time.sleep(5)
+    drone.goto(x=1, y=1, z=2, yaw=0, wait=True)
+    drone.land()
 
 if __name__ == "__main__":
     main()
